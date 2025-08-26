b32c1d106af0f104dfd9829164ce8b5fa0e903b8
Deep-paul74 <pauldeep377@gmail.com>
2025-08-27 02:10:49 +0600

    assignment

---
+++ insertionSort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nBefore sorting: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("After sorting: ");
+    printArray(arr, n);
+
+    return 0;
+}
