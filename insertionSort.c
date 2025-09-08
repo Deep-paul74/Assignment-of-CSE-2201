b6a6b35d78555337320e3b66579dfde85b8950aa
Deep-paul74 <pauldeep377@gmail.com>
2025-09-08 20:15:24 +0600

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
+
+// Insertion Sort function
+
 void insertionSort(int arr[], int n) {
     int i, key, j;
+    // 1 to n-1 loop
     for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+        key = arr[i];    // key = present element
+        j = i - 1;       //  ager index er element er sathe compare
 
+          // joto somoy ager element key theke large , right e sorano hosse
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
+
+          // key ke correct  position e bosano hosse
         arr[j + 1] = key;
     }
 }
+
+
+// Array print korar function
 
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++)
@@ -42,3 +52,4 @@ int main() {
 
     return 0;
 }
+
